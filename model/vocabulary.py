56a5c3430c2c5ba9b6c014016a1411621b2e34be
Andrea-Caminiti <caminiti.1924053@studenti.uniroma1.it>
2025-01-19 19:24:11 +0100

    Added Tokenizer class

--- model/vocabulary.py
+++ model/vocabulary.py
@@ -2,6 +2,7 @@ import numpy as np
 import torch
 
 from typing import Dict
+import re
 
 class TokenAlreadyInVocabError(Exception):
     pass
@@ -121,4 +122,43 @@ class Vocabulary:
     
 
 class Tokenizer:
+    '''
+    Class to handle tokenization of SMILES strings
+    '''
+    def __init__(self):
+        self.REGEXPS = {
+        "brackets": re.compile(r"(\[[^\]]*\])"),
+        "2_ring_nums": re.compile(r"(%\d{2})"),
+        "brcl": re.compile(r"(Br|Cl)")
+        }
+        self.ORDER = ["brackets", "2_ring_nums", "brcl"]
     
+    def tokenize(self, data, with_begin_and_end=True):
+        """Tokenizes a SMILES string."""
+        def split_by(data, regexps):
+            if not regexps:
+                return list(data)
+            regexp = self.REGEXPS[regexps[0]]
+            splitted = regexp.split(data)
+            tokens = []
+            for i, split in enumerate(splitted):
+                if i % 2 == 0:
+                    tokens += split_by(split, regexps[1:])
+                else:
+                    tokens.append(split)
+            return tokens
+
+        tokens = split_by(data, self.ORDER)
+        if with_begin_and_end:
+            tokens = ["^"] + tokens + ["$"]
+        return tokens
+
+    def untokenize(self, tokens):
+        """Untokenizes a SMILES string."""
+        smi = ""
+        for token in tokens:
+            if token == "$":
+                break
+            if token != "^":
+                smi += token
+        return smi
